da1e9ac19f68c529fbc48209f39cdc0f06913aa9
Tunghome <tung0963002862@gmail.com>
2024-03-31 04:17:49 +0700

    Init UrlMiddleware

---
+++ zf-ext/Middleware/UrlMiddleware.php
@@ -0,0 +1,15 @@
+<?php
+namespace Zf\Ext\Middleware;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class UrlMiddleware implements MiddlewareInterface
+{
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        return $handler->handle($request);
+    }
+}
+?>
